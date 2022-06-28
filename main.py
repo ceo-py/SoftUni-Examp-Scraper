d4db9896abe91b79d451035f99a86b16a9a30bca
ceo-py <sfmf@abv.bg>
2022-06-28 12:27:35 +0200

    json data first page

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ options = Options()
 options.headless = False
 PATH = r"D:\PyCharm\amazon items\chromedriver.exe"
 url = "https://judge.softuni.org/Contests/Compete/Results/Simple/3477"
-user_name = "234234"
-password = "23434"
+user_name = "mljam"
+password = "123456yU"
 driver = webdriver.Chrome(PATH, options=options)
 driver.get(url)
+
 username_find = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/section/form/div[2]/div/input')
 username_find.send_keys(user_name)
 password_find = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/section/form/div[3]/div/input')
@@ -20,9 +21,8 @@ submit_button = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/section
 submit_button.click()
 html_data = driver.page_source
 r_html = HTML(html=html_data)
+
 examp_info = {}
-
-
 def get_info_from_web():
     for contestant in range(1, 101):
         total_score = int(r_html.xpath(f'/html/body/div[2]/div/table/tbody/tr[{contestant}]/td[19]', first=True).text)
@@ -30,15 +30,27 @@ def get_info_from_web():
         examp_info["total_score"].append(total_score)
         for examp in range(4, 19):
             task_name = r_html.xpath(f'/html/body/div[2]/div/table/thead/tr/th[{examp}]', first=True).text
-            task_score = r_html.xpath(f'/html/body/div[2]/div/table/tbody/tr[{contestant}]/td[{examp}]',
-                                      first=True).text
+            task_score = r_html.xpath(f'/html/body/div[2]/div/table/tbody/tr[{contestant}]/td[{examp}]', first=True).text
             examp_info[task_name] = examp_info.get(task_name, [])
             if task_score.isdigit():
                 examp_info[task_name].append(int(task_score))
 
 
 get_info_from_web()
-
+# print(examp_info)
+# print(len(examp_info['total_score']))
+#
+# first_task = '/html/body/div[2]/div/table/thead/tr/th[4]'
+# last_tank = '/html/body/div[2]/div/table/thead/tr/th[18]'
+#
+# first_contest_first_task = '/html/body/div[2]/div/table/tbody/tr[1]/td[4]'
+# first_contest_last_task = '/html/body/div[2]/div/table/tbody/tr[1]/td[18]'
+# first_contest_total_score = '/html/body/div[2]/div/table/tbody/tr[1]/td[19]'
+#
+# second_contest_first_task = '/html/body/div[2]/div/table/tbody/tr[2]/td[4]'
+# second_contest_last_task = '/html/body/div[2]/div/table/tbody/tr[2]/td[18]'
+# second_contest_total_score = '/html/body/div[2]/div/table/tbody/tr[2]/td[19]'
+#
 
 def write_json(data, filename="examp_info.json"):
     with open(filename, "w", encoding='utf-8') as x:

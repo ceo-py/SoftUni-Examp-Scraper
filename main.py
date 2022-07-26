35cccf19b4dbb01df7051adc6ef1bf4288d5b5bb
ceo-py <sfmf@abv.bg>
2022-07-26 12:38:21 +0200

    basic statistics ref

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import json
 options = Options()
 options.headless = False
 PATH = r"D:\PyCharm\amazon items\chromedriver.exe"
-url = "https://judge.softuni.org/Contests/Compete/Results/Simple/3477"
-user_name = "234324"
-password = "34344"
+url = "https://judge.softuni.org/Contests/Compete/Results/Simple/3537?page=5"
+user_name = "234234324"
+password = "234234234324"
 driver = webdriver.Chrome(PATH, options=options)
 driver.get(url)
 
@@ -22,31 +22,33 @@ submit_button.click()
 html_data = driver.page_source
 r_html = HTML(html=html_data)
 
-examp_info = {}
-def get_info_from_web():
+
+def get_info_from_web(examp_info):
     for contestant in range(1, 101):
-        total_score = int(r_html.xpath(f'/html/body/div[2]/div/table/tbody/tr[{contestant}]/td[19]', first=True).text)
-        examp_info["total_score"] = examp_info.get("total_score", [])
-        examp_info["total_score"].append(total_score)
-        for examp in range(4, 19):
-            task_name = r_html.xpath(f'/html/body/div[2]/div/table/thead/tr/th[{examp}]', first=True).text
-            task_score = r_html.xpath(f'/html/body/div[2]/div/table/tbody/tr[{contestant}]/td[{examp}]', first=True).text
-            examp_info[task_name] = examp_info.get(task_name, [])
-            if task_score.isdigit():
-                examp_info[task_name].append(int(task_score))
+        try:
+            total_score = int(r_html.xpath(f'/html/body/div[2]/div/table/tbody/tr[{contestant}]/td[20]', first=True).text)
+            examp_info["total_score"] = examp_info.get("total_score", [])
+            examp_info["total_score"].append(total_score)
+            for examp in range(4, 20):
+                task_name = r_html.xpath(f'/html/body/div[2]/div/table/thead/tr/th[{examp}]', first=True).text
+                task_score = r_html.xpath(f'/html/body/div[2]/div/table/tbody/tr[{contestant}]/td[{examp}]',
+                                          first=True).text
+                examp_info[task_name] = examp_info.get(task_name, [])
+                if task_score.isdigit():
+                    examp_info[task_name].append(int(task_score))
+        except:
+            return
 
 
-get_info_from_web()
-
-
-def write_json(data, filename="examp_info.json"):
+def write_json(data, filename="basic_23_24_july_2022.json"):
     with open(filename, "w", encoding='utf-8') as x:
         json.dump(data, x, indent=9)
 
 
-with open("examp_info.json", "r+", encoding='utf-8') as json_file:
+with open("basic_23_24_july_2022.json", "r+", encoding='utf-8') as json_file:
     data = json.load(json_file)
-    data_info = data["SoftUni"]["Info"]
-    data_info.append(examp_info)
+    data_info = data
+    get_info_from_web(data_info)
+
 
 write_json(data)

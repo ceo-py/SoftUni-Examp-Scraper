4be3837f68d7d264c43d9e46915a4f63b9f5d245
ceo-py <sfmf@abv.bg>
2022-07-26 12:37:50 +0200

    basic statistics ref

--- basic_examp_info.py
+++ basic_examp_info.py
@@ -1,16 +1,32 @@
 import json
 
-with open("basic_examp_info.json", "r+", encoding='utf-8') as json_file:
+with open("basic_23_24_july_2022.json", "r", encoding='utf-8') as json_file:
     data = json.load(json_file)
-    data_info = data["SoftUni"]["Info"][0]
+    data_info = data
 
 
 def task_frequency(data_info):
     print("\nHardest task : \n")
     for task_name, task_scores in sorted(data_info.items(), key=lambda x: (sum(x[1]) / len(x[1]))):
         if task_name != 'total_score':
-            print(f"{task_name} average score {(sum(task_scores) / len(task_scores)):.2f}% "
+            print(f"{task_name} average score {(sum(task_scores) / len(task_scores)):.2f} "
                   f"/ 100, with {len(task_scores)} contestants attempt the task!")
+
+
+def task_with_max_score(data_info):
+    print("\nTask with highest score: \n")
+    num_high_score = {}
+    for task_name, task_scores in data_info.items():
+        if task_name != 'total_score':
+            num_high_score[task_name] = num_high_score.get(task_name, 0)
+            for score in task_scores:
+                if score == 100:
+                    num_high_score[task_name] += 1
+    for task_name, task_score in sorted(num_high_score.items(), key= lambda x: -x[1]):
+        print(f"{task_name} has {task_score} contestants with maximum score out of {len(data_info[task_name])} attempted the task!")
+
+
+
 
 
 students_info = {"6.00": 0,
@@ -41,6 +57,7 @@ for score, number_people in students_info.items():
     print(f"{score} - {number_people} / {total_contestants}, {((number_people / total_contestants) * 100):.2f}%")
 
 
-print(f"\n{((sum(list(students_info.values())[:4])/ total_contestants)*100):.2f}% passed the test, Congrats !!!")
+print(f"\n{((sum(list(students_info.values())[:3])/ total_contestants)*100):.2f}% passed the test, Congrats !!!")
 task_frequency(data_info)
+task_with_max_score(data_info)
 
